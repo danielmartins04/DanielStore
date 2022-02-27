3c674863933ac3bd89da81b9ea6ff3a170771310
Daniel Martins <freitasdaniel39@gmail.com>
2022-02-27 11:09:57 -0300

    modelando o pedido

--- DanielStore.Domain/StoreContext/Entities/Customer.cs
+++ DanielStore.Domain/StoreContext/Entities/Customer.cs
@@ -1,11 +1,13 @@
-using System;
 using System.Collections.Generic;
 using DanielStore.Domain.StoreContext.ValueObjects;
+using System.Linq;
 
 namespace DanielStore.Domain.StoreContext.Entities
 {
     public class Customer
     {
+        private readonly IList<Address> _addresses;
+
         public Customer(
             Name name,
             Document document,
@@ -16,14 +18,19 @@ namespace DanielStore.Domain.StoreContext.Entities
             Document = document;
             Email = email;
             Phone = phone;
-            Addresses = new List<Address>();
+            _addresses = new List<Address>();
         }
 
         public Name Name { get; private set; }
         public Document Document { get; private set; }
         public Email Email { get; private set; }
         public string Phone { get; private set; }
-        public IReadOnlyCollection<Address> Addresses { get; private set; }
+        public IReadOnlyCollection<Address> Addresses => _addresses.ToArray();
+
+        public void AddAddress(Address address)
+        {
+            _addresses.Add(address);
+        }
 
         public override string ToString()
         {
