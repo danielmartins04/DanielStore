6f44afa4a05539265ee1dcc93a6855e90b6d6c26
Daniel Martins <freitasdaniel39@gmail.com>
2022-03-01 14:52:08 -0300

    testando ValueObjects

---
+++ DanielStore.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,30 @@
+using DanielStore.Domain.StoreContext.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DanielStore.Tests
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        private Document validDocument;
+        private Document invalidDocument;
+
+        public DocumentTests()
+        {
+            validDocument = new Document("12345678910");
+            invalidDocument = new Document("06507398330");
+        }
+
+        [TestMethod]
+        public void ShouldReturnNotificationWhenDocumentIsNotValid()
+        {
+            Assert.AreEqual(false, validDocument.IsValid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnNotificationWhenDocumentIsValid()
+        {
+            Assert.AreEqual(true, invalidDocument.IsValid);
+        }
+    }
+}
---
+++ DanielStore.Tests/ValueObjects/NameTests.cs
@@ -0,0 +1,30 @@
+using DanielStore.Domain.StoreContext.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DanielStore.Tests
+{
+    [TestClass]
+    public class NameTests
+    {
+        private Name validName;
+        private Name invalidName;
+
+        public NameTests()
+        {
+            validName = new Name("", "Martins");
+            invalidName = new Name("Daniel", "Martins");
+        }
+
+        [TestMethod]
+        public void ShouldReturnNotificationWhenNameIsNotValid()
+        {
+            Assert.AreEqual(false, validName.IsValid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnNotificationWhenNameIsValid()
+        {
+            Assert.AreEqual(true, invalidName.IsValid);
+        }
+    }
+}
