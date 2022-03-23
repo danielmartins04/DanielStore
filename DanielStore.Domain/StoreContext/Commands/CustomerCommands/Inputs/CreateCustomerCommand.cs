e2e9d0daf4cb54b570763e0ad4048e123d62b03a
Daniel Martins <freitasdaniel39@gmail.com>
2022-03-22 23:13:48 -0300

    lidando com Fail Fast Validation

--- DanielStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/CreateCustomerCommand.cs
+++ DanielStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/CreateCustomerCommand.cs
@@ -1,11 +1,27 @@
+using FluentValidator;
+using FluentValidator.Validation;
+
 namespace DanielStore.Domain.StoreContext.CustomerCommands.Inputs
 {
-    public class CreateCustomerCommand
+    public class CreateCustomerCommand : Notifiable
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Document { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+
+        public bool Valid()
+        {
+            AddNotifications(new ValidationContract()
+                .HasMinLen(FirstName, 3, "FirstName", "O nome deve conter pelo menos 3 carateres.")
+                .HasMaxLen(FirstName, 30, "FirstName", "O nome deve conter no máximo 30 caracteres.")
+                .HasMinLen(LastName, 3, "LastName", "O sobrenome deve ter no mínimo 3 caracters")
+                .HasMaxLen(LastName, 30, "LastName", "O sobrenome deve ter no máximo 30 caracteres.")
+                .IsEmail(Email, "Email", "E-mail inválido")
+                .HasLen(Document, 11, "Document", "CPF inválido")
+            );
+            return IsValid;
+        }
     }
 }
