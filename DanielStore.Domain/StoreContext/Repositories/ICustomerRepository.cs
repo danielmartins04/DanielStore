1d1136babe965dd20b9f84f362d6a19d62a01f21
Daniel Martins <freitasdaniel39@gmail.com>
2022-03-25 15:29:38 -0300

    criando Customer Repository

---
+++ DanielStore.Domain/StoreContext/Repositories/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+using DanielStore.Domain.StoreContext.Entities;
+
+namespace DanielStore.Domain.StoreContext.Repositories
+{
+    public interface ICustomerRepository
+    {
+        bool CheckDocument(string document);
+        bool CheckEmail(string email);
+        void Save(Customer customer);
+    }
+}
